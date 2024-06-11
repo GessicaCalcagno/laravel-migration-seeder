3c274fd3a50802d912f934834715073b03afddab
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-06-11 16:36:08 +0200

    Add: train routes

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
-    //
+    public function index(){
+        $today = Carbon::today();
+        $trains = Train::where('orario_di_partenza', '>=', $today->format('H:i:s'))
+        ->get();
+        dd($trains);
+        return view('trains.index', compact('trains'));
+    }
 }
