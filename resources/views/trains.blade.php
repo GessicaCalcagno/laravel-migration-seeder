ce3c7845a3f1df35a10668d6244459ae2dc327ec
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-06-11 18:15:05 +0200

    Add: stamp table

---
+++ resources/views/trains.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <h1>Treni</h1>
+        {{-- ________DATA?_____ --}}
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Azienda</th>
+                <th scope="col">Stazione di Partenza</th>
+                <th scope="col">Stazione di Arrivo</th>
+                <th scope="col">Orario di Partenza</th>
+                <th scope="col">Orario di Arrivo</th>
+                <th scope="col">Codice Treno</th>
+                <th scope="col">Numero Carrozze</th>
+                <th scope="col">In Orario</th>
+                <th scope="col">Cancellato</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr> @foreach ($trains as $train)
+                <tr>
+                    <th scope="row">{{ $train->azienda }}</td>
+                    <td>{{ $train->stazione_di_partenza }}</td>
+                    <td>{{ $train->stazione_di_arrivo }}</td>
+                    <td>{{ $train->orario_di_partenza }}</td>
+                    <td>{{ $train->orario_di_arrivo }}</td>
+                    <td>{{ $train->codice_treno }}</td>
+                    <td>{{ $train->numero_carrozze }}</td>
+                    <td>{{ $train->in_orario ? 'Sì' : 'No' }}</td>
+                    <td>{{ $train->cancellato ? 'Sì' : 'No' }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+          </table>
+    </div>
+@endsection
